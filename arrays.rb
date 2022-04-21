936a2b8ce5a12a65e4c5aca9fe5d39b197126f6c
Samuel Bacon <sbacon803@gmail.com>
2022-04-20 19:35:30 -0600

    Add array methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ Dogs = ["Dobermans", "Beagles", "Dachsunds", "Retrievers"]
 Dog_years = [28, 21, 84, 70]
 Ages = [4, 3.75, 12, 9.5]
 House_trained = [true, false, true, true]
+
+# Pop removes the last element (Retrievers)
+Dogs.pop
+
+# Push 'pushes' more elements into the end of the array
+Dogs.push("Chihuahuas", "Great Danes")
+
+# Shift removes the first element and returns it
+Dogs.shift
+
+# Unshift sticks element into front of the array list
+Dogs.unshift("Akitas", "Mutts")
